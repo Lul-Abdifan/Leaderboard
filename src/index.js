89a6a4cf5dbc203f84f4cd9751fd70703b9fff79
hope <tasanbannu@gmail.com>
2023-04-12 15:24:56 -0400

    Update refresh onclick button  functionality

--- src/index.js
+++ src/index.js
@@ -3,23 +3,16 @@ import { table } from "./modules/variables";
 import { postData, fetchData } from "./modules/api";
 import { refresher,addScore } from "./modules/variables";
 
-
-
-refresher.onclick = () => {
-  table.innerHTML = "";
-  fetchData().then((res) => {
-    res.result.sort((a, b) =>b.score - a.score).forEach((data) => {
-      render(data);
-    });
-  });
+addScore.onsubmit = (e) => {
+  e.preventDefault();
+  const name = e.target.name.value;
+  const score = e.target.score.value;
+  postData({ user: name, score: score });
+  e.target.name.value = '';
+  e.target.score.value = '';
 };
-const render = (data) => {
-    const tr = document.createElement("tr");
-    const td = document.createElement("td");
-    td.innerHTML = `${data.user}:${data.score}`;
-    tr.appendChild(td);
-    table.append(tr);
-  };
+
+
 
 
 
