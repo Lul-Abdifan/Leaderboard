b67f19b81be168cba2275e0cee24119486741a05
hope <tasanbannu@gmail.com>
2023-04-12 15:24:08 -0400

    Add form submit functionality

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import { table } from "./modules/variables";
 import { postData, fetchData } from "./modules/api";
 import { refresher,addScore } from "./modules/variables";
 
-addScore.onsubmit = (e) => {
-  e.preventDefault();
-  const name = e.target.name.value;
-  const score = e.target.score.value;
-  postData({ user: name, score: score });
-  e.target.name.value = '';
-  e.target.score.value = '';
-};
+
 
 refresher.onclick = () => {
   table.innerHTML = "";
